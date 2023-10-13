bd66650cc879a47881c2ee8a683ee5eb2d900a83
shlok0070 <shlok0070@gmail.com>
2023-10-14 00:26:59 +0530

    added SQL query to get products

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const db = require('./util/database');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,6 +13,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('SELECT * FROM products').then().catch();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
